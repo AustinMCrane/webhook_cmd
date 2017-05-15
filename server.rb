0fda3201230a66e062eb79b7b31db84e85e2f4fd
Austin M Crane <austin.crane@ennovar.wichita.edu>
2017-05-15 11:05:09 -0500

    added server that seems to be working

---
+++ server.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'json'
+
+def get_cmd_for_key(key, passkey)
+    file = File.read('./config.json')
+    data_hash = JSON.parse(file)
+    cmd_pair = data_hash[key]
+    unless cmd_pair
+        return nil
+    end
+    if cmd_pair['passkey'] == passkey
+        return cmd_pair
+    end
+    return 'Error'
+end
+
+get '/hook' do
+    cmd_pair = get_cmd_for_key('test', 'test')
+    value = %x(#{cmd_pair['cmd']})
+    value
+end
